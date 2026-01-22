1d3c4bd2ad9fff2a51f7f9f44bc9c11acf36f5c6
Hugo <hsanchezgallardo.guadalupe@alumnado.fundacionloyola.net>
2026-01-22 18:58:06 +0100

    Subiendo mi proyecto inicial a GitHub

--- index.js
+++ index.js
@@ -134,4 +134,6 @@ app.post('/api/voz', upload.single('audio'), async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Servidor en http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
